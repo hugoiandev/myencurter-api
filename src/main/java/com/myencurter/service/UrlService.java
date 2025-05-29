2e39ced64156e1b542ab722b4a3a10638e6b3542
Hugo Ian da Silva <ahugaao@gmail.com>
2025-05-29 00:14:30 -0300

    Initial tests

--- src/main/java/com/myencurter/service/UrlService.java
+++ src/main/java/com/myencurter/service/UrlService.java
@@ -4,11 +4,9 @@ import com.myencurter.model.Url;
 import com.myencurter.model.User;
 import com.myencurter.repository.UrlRepository;
 import com.myencurter.repository.UserRepository;
+import com.myencurter.util.HashGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.math.BigInteger;
-import java.security.SecureRandom;
 import java.util.List;
 
 @Service
@@ -16,16 +14,18 @@ public class UrlService {
 
     @Autowired
     private UrlRepository urlRepository;
+
     @Autowired
     private UserRepository userRepository;
 
-    private static final String BASE62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    @Autowired
+    private HashGenerator hashGenerator;
 
     public Url saveUrl(String url, String username) {
 
-        User user = userRepository.findByEmail(username).orElseThrow(() -> new RuntimeException("User Not Found."));
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
 
-        String shortId = generateHash();
+        String shortId = hashGenerator.generateHash();
 
         Url urlEntity = new Url();
         urlEntity.setId(shortId);
@@ -42,30 +42,9 @@ public class UrlService {
 
     public List<Url> getAllUrlsByUser(String username) {
 
-        User user = userRepository.findByEmail(username).orElseThrow(() -> new RuntimeException("User Not Found."));
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
 
         return urlRepository.findAllByUserId(user.getId());
-    }
-
-    private String generateHash() {
-
-        int length = 7;
-
-        SecureRandom random = new SecureRandom();
-        BigInteger number = new BigInteger(130, random);
-        StringBuilder sb = new StringBuilder();
-
-        while (number.compareTo(BigInteger.ZERO) > 0 && sb.length() < length) {
-            int remainder = number.mod(BigInteger.valueOf(62)).intValue();
-            sb.append(BASE62.charAt(remainder));
-            number = number.divide(BigInteger.valueOf(62));
-        }
-
-        while (sb.length() < length) {
-            sb.append(BASE62.charAt(random.nextInt(62)));
-        }
-
-        return sb.toString();
     }
 
 }
--- src/test/java/com/myencurter/service/UrlServiceTest.java
+++ src/test/java/com/myencurter/service/UrlServiceTest.java
@@ -1,14 +1,25 @@
 package com.myencurter.service;
 
+import com.myencurter.model.Url;
+import com.myencurter.model.User;
 import com.myencurter.repository.UrlRepository;
+import com.myencurter.repository.UserRepository;
+import com.myencurter.util.HashGenerator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UrlServiceTest {
@@ -16,6 +27,129 @@ class UrlServiceTest {
     @Mock
     private UrlRepository urlRepository;
 
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private HashGenerator hashGenerator;
+
     @InjectMocks
     private UrlService urlService;
+
+    @Captor
+    private ArgumentCaptor<Url> urlSaveArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<String> urlGetArgumentCaptor;
+
+    @Nested
+    class SaveUrl {
+
+        @Test
+        @DisplayName("Deve criar e retornar id e url.")
+        void shouldSaveUrlAndReturnUrl() {
+
+            User user = new User();
+            user.setId(1L);
+            user.setName("Hugo Ian da Silva");
+            user.setEmail("ahugaao@gmail.com");
+
+            Url url = new Url();
+            url.setUrl("https://www.google.com");
+            url.setId("Hyefj9d");
+
+            when(userRepository.findByEmail("ahugaao@gmail.com")).thenReturn(Optional.of(user));
+
+            when(hashGenerator.generateHash()).thenReturn("Hyefj9d");
+
+            doReturn(url).when(urlRepository).save(urlSaveArgumentCaptor.capture());
+
+            Url result = urlService.saveUrl(url.getUrl(), user.getEmail());
+
+            assertNotNull(result);
+            assertEquals(result.getId(), urlSaveArgumentCaptor.getValue().getId());
+            assertEquals(result.getUrl(), urlSaveArgumentCaptor.getValue().getUrl());
+
+        }
+
+        @Test
+        @DisplayName("Deve lançar uma exceção quando não encontrar um usuário.")
+        void shouldThrowExceptionWhenUserNotFound() {
+
+            when(userRepository.findByEmail(any())).thenReturn(Optional.empty());
+
+            assertThrows(RuntimeException.class, () -> urlService.saveUrl("", ""));
+
+        }
+    }
+
+    @Nested
+    class GetUrl {
+
+        @Test
+        @DisplayName("Deve retornar uma url por id.")
+        void shouldReturnUrlById() {
+
+            Url url = new Url();
+            url.setUrl("https://www.google.com");
+            url.setId("Hyefj9d");
+
+            when(urlRepository.findById(urlGetArgumentCaptor.capture())).thenReturn(Optional.of(url));
+
+            Url result = urlService.getUrl(url.getId());
+
+            assertNotNull(result);
+            assertEquals(result.getId(), urlGetArgumentCaptor.getValue());
+        }
+
+        @Test
+        @DisplayName("Deve lançar uma exceção quando não encontrar a url com o id.")
+        void shouldThrowExceptionWhenUrlNotFound() {
+
+            when(urlRepository.findById(any())).thenReturn(Optional.empty());
+
+            assertThrows(RuntimeException.class, () -> urlService.getUrl(""));
+
+        }
+
+    }
+
+    @Nested
+    class GetAllUrls {
+
+        @Test
+        @DisplayName("Deve retornar uma lista de urls do usuário.")
+        void shouldReturnAllUrls() {
+            Url url1 = new Url();
+            url1.setId("Hyefj9d");
+            url1.setUrl("https://www.google.com");
+
+            Url url2 = new Url();
+            url2.setId("Hywesdd");
+            url2.setUrl("https://www.linkedin.com");
+
+            List<Url> urls = Arrays.asList(url1, url2);
+
+            User user = new User();
+            user.setId(1L);
+
+            when(userRepository.findByEmail("ahugaao@gmail.com")).thenReturn(Optional.of(user));
+
+            when(urlRepository.findAllByUserId(user.getId())).thenReturn(urls);
+
+            List<Url> result = urlService.getAllUrlsByUser("ahugaao@gmail.com");
+
+            assertNotNull(result);
+            assertEquals(2, result.size());
+            assertEquals("https://www.google.com", result.get(0).getUrl());
+            assertEquals("https://www.linkedin.com", result.get(1).getUrl());
+
+        }
+
+        @Test
+        @DisplayName("Deve lançar uma lista vazia quando não encontrar urls.")
+        void shouldThrowExceptionWhenUrlsEmpty() {
+
+        }
+    }
 }
